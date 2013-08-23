4062090a51fcc3b50e3d6cf4f4a08ce9289ffb6c
fafaisland <fafaisland@gmail.com>
2013-08-22 22:28:51 -0700

    Navigation Drawer Done

--- TimingMate/gen/com/example/timingmate/R.java
+++ TimingMate/gen/com/example/timingmate/R.java
@@ -21,7 +21,9 @@ public final class R {
         public static final int activity_vertical_margin=0x7f040001;
     }
     public static final class drawable {
-        public static final int ic_launcher=0x7f020000;
+        public static final int drawer_shadow=0x7f020000;
+        public static final int ic_drawer=0x7f020001;
+        public static final int ic_launcher=0x7f020002;
     }
     public static final class id {
         public static final int action_settings=0x7f080002;
@@ -40,6 +42,8 @@ public final class R {
         public static final int app_name=0x7f050000;
         public static final int drawer_text=0x7f050003;
         public static final int hello_world=0x7f050002;
+        public static final int menu_close=0x7f050005;
+        public static final int menu_open=0x7f050004;
     }
     public static final class style {
         /** 
