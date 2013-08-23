c9e81d68cdef8e18e7da6dc841105dfa058055bd
fafaisland <fafaisland@gmail.com>
2013-08-23 14:38:22 -0700

    Actionbar Menu
    add delete, finish, edit button

--- TimingMate/gen/com/example/timingmate/R.java
+++ TimingMate/gen/com/example/timingmate/R.java
@@ -26,7 +26,10 @@ public final class R {
         public static final int ic_launcher=0x7f020002;
     }
     public static final class id {
-        public static final int action_settings=0x7f080002;
+        public static final int action_delete_task=0x7f080003;
+        public static final int action_edit_task=0x7f080004;
+        public static final int action_finishToggle_task=0x7f080002;
+        public static final int action_settings=0x7f080005;
         public static final int drawer=0x7f080001;
         public static final int drawer_layout=0x7f080000;
     }
@@ -38,6 +41,9 @@ public final class R {
         public static final int main=0x7f070000;
     }
     public static final class string {
+        public static final int action_delete_task=0x7f050007;
+        public static final int action_edit_task=0x7f050008;
+        public static final int action_finishToggle_task=0x7f050006;
         public static final int action_settings=0x7f050001;
         public static final int app_name=0x7f050000;
         public static final int drawer_text=0x7f050003;
