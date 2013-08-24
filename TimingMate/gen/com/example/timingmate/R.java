2fac47e9d04deb0ce18b99012a493bc9b6551717
fafaisland <fafaisland@gmail.com>
2013-08-23 22:28:27 -0700

    Timer View UI

--- TimingMate/gen/com/example/timingmate/R.java
+++ TimingMate/gen/com/example/timingmate/R.java
@@ -26,19 +26,22 @@ public final class R {
         public static final int ic_launcher=0x7f020002;
     }
     public static final class id {
-        public static final int action_create_task=0x7f080008;
-        public static final int action_delete_task=0x7f08000a;
-        public static final int action_edit_task=0x7f08000b;
-        public static final int action_finishToggle_task=0x7f080009;
-        public static final int action_settings=0x7f08000c;
+        public static final int action_create_task=0x7f08000b;
+        public static final int action_delete_task=0x7f08000d;
+        public static final int action_edit_task=0x7f08000e;
+        public static final int action_finishToggle_task=0x7f08000c;
+        public static final int action_settings=0x7f08000f;
+        public static final int analogClock1=0x7f080008;
         public static final int button1=0x7f080000;
         public static final int button2=0x7f080001;
         public static final int button3=0x7f080004;
         public static final int button4=0x7f080005;
-        public static final int drawer=0x7f080007;
+        public static final int drawer=0x7f08000a;
         public static final int drawer_layout=0x7f080006;
         public static final int editText1=0x7f080003;
         public static final int textView1=0x7f080002;
+        public static final int textView2=0x7f080007;
+        public static final int textView3=0x7f080009;
     }
     public static final class layout {
         public static final int activity_create_task=0x7f030000;
@@ -55,6 +58,8 @@ public final class R {
         public static final int action_finishToggle_task=0x7f050006;
         public static final int action_settings=0x7f050001;
         public static final int app_name=0x7f050000;
+        public static final int btn_createTaskView_cancel=0x7f05000c;
+        public static final int btn_createTaskView_save=0x7f05000d;
         public static final int drawer_text=0x7f050003;
         public static final int hello_world=0x7f050002;
         public static final int menu_close=0x7f050005;
