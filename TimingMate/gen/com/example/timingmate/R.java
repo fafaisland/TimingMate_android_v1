8096ad7fb5261dc666ba3963fc5018f733b3e03a
fafaisland <fafaisland@gmail.com>
2013-08-24 17:20:46 -0700

    Make List Layout scrollable
    LinearLayout outside list view

--- TimingMate/gen/com/example/timingmate/R.java
+++ TimingMate/gen/com/example/timingmate/R.java
@@ -26,22 +26,23 @@ public final class R {
         public static final int ic_launcher=0x7f020002;
     }
     public static final class id {
-        public static final int action_create_task=0x7f08000b;
-        public static final int action_delete_task=0x7f08000d;
-        public static final int action_edit_task=0x7f08000e;
-        public static final int action_finishToggle_task=0x7f08000c;
-        public static final int action_settings=0x7f08000f;
-        public static final int analogClock1=0x7f080009;
+        public static final int action_create_task=0x7f08000c;
+        public static final int action_delete_task=0x7f08000e;
+        public static final int action_edit_task=0x7f08000f;
+        public static final int action_finishToggle_task=0x7f08000d;
+        public static final int action_settings=0x7f080010;
+        public static final int analogClock1=0x7f08000a;
         public static final int button1=0x7f080000;
         public static final int button2=0x7f080001;
         public static final int button3=0x7f080004;
         public static final int button4=0x7f080005;
-        public static final int drawer_layout=0x7f080007;
+        public static final int drawer_layout=0x7f080008;
         public static final int editText1=0x7f080003;
+        public static final int list=0x7f080007;
         public static final int lists_layout=0x7f080006;
         public static final int textView1=0x7f080002;
-        public static final int textView2=0x7f080008;
-        public static final int textView3=0x7f08000a;
+        public static final int textView2=0x7f080009;
+        public static final int textView3=0x7f08000b;
     }
     public static final class layout {
         public static final int activity_create_task=0x7f030000;
